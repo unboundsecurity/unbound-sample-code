1305555e1755d1222ce33b069f3393242c5c6b24
Adam Ilan <adam.ilan@unboundtech.com>
2020-12-25 19:11:06 +0200

    remove infura project id

--- blockchain/ethereum/with-ukc/ukc-test-ethereum.js
+++ blockchain/ethereum/with-ukc/ukc-test-ethereum.js
@@ -1,4 +1,6 @@
-// This is a simple program that demonstrates the use of UKC REST Api for ethereum transaction sign
+/**
+ * This sample shows how to create an Ethereum transaction and sign it with a key stored in UKC
+ */
 'use strict'
 
 const ethUtil = require('ethereumjs-util')
@@ -11,13 +13,13 @@ const axios = require('axios');
 // This should be used only for testing.
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-// The provider url for web3
-// If you don't have one, get it for free from by opening an account at https://infura.io
+// Replace the provider url for web3 with the url from your Infura account.
+// Get it for free by opening an account at https://infura.io
 // It is recommended to use Ropsten testnet for tests
-const InfuraEndpoint = 'https://ropsten.infura.io/v3/431778d1fb804338ace2e1a5d509fb62';
+const InfuraEndpoint = 'https://ropsten.infura.io/v3/431770000000038ace2e1a5d509fb62';
 
 // UKC server configuration
-// Make sure you configure your UKC url, port and user credentials
+// Make sure you configure your UKC partition, url, port and user credentials
 const UKC_PARTITION = 'test'
 const UKC_URL = 'https://localhost:8443'
 const UKC_USER = 'ethuser'
@@ -25,17 +27,17 @@ const UKC_PASSWORD = 'Password1!'
 
 // The name or UID of the key to use for Eth address
 // The key must be of type ECC and with curve ECC-SECP_256K_1
-const UKC_KEY_UID = 'eth' // 3eeca3c05c982f4d'  // signing key
+const UKC_KEY_UID = 'eth';
 
 const recipientAddress = '0xDC839532D5ab3e7Cc958b4F86d733CbD1617ee4A';
-const amountToSendInEth = "0.0004"; // amount in Ethereum
+const amountToSendInEth = "0.0004";
 
 const ukcClient = axios.create({
 	baseURL: UKC_URL,
 	timeout: 1000,
-	headers: {
-		'Content-Type': 'application/json',
-		'Authorization': 'Basic' + Buffer.from(UKC_USER + '@' + UKC_PARTITION + ":" + UKC_PASSWORD).toString("base64"),
+	auth: {
+		username: `${UKC_USER}@${UKC_PARTITION}`,
+		password: UKC_PASSWORD
 	}
 });
 
@@ -50,7 +52,7 @@ async function test() {
 	var key = new ECKey(response.data.keyData, 'pem');
 	var fromAddress = ethAddressFromPubKeyBuffer(Buffer.concat([key.x, key.y]));;
 
-	// Configures web3 connection to the ethereum network
+	// Prepare the unsigned transaction
 	var web3 = new Web3(new Web3.providers.HttpProvider(InfuraEndpoint));
 	var balanceBefore = await web3.eth.getBalance(fromAddress);
 	console.log("Sender address: " + fromAddress);
@@ -68,13 +70,13 @@ async function test() {
 		chain
 	});
 
+	// Get signature from UKC and apply to the transaction
 	var txHash = tx.hash(false).toString('hex');
 	response = await ukcClient.post(`/api/v1/keys/${UKC_KEY_UID}/sign`, {
 		data: txHash,
 		dataEncoding: "HEX",
 		doHash: false,
 	});
-
 	var sig = Buffer.from(response.data.signature, "base64");
 	const sigStruct = {
 		r: sig.slice(0, 32),
@@ -84,29 +86,30 @@ async function test() {
 	// According to https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md
 	const recover = (v) => ethAddressFromPubKeyBuffer(ethUtil.ecrecover(tx.hash(false), v, sigStruct.r, sigStruct.s));
 	var recoveryParam = [27, 28].find(v => fromAddress === recover(v));
-	sigStruct.v = Buffer.from([chain * 2 + (recoveryParam -27) + 35]);
+	sigStruct.v = Buffer.from([chain * 2 + (recoveryParam - 27) + 35]);
 	Object.assign(tx, sigStruct);
 	console.log(tx.toJSON(true));
+	if (!tx.validate()) throw "Invalid signature";
 
-	if(!tx.validate()) throw "Invalid signature";
-
+	// Broadcast the signed transaction to the Eth network
 	var serializedTx = '0x' + tx.serialize().toString('hex');
 	console.log("\nBroadcasting transaction...");
 	var transaction = await web3.eth.sendSignedTransaction(serializedTx);
 
+	// Output the URL to the transaction in block-explorer
 	var txUrl;
-	switch(chain) {
+	switch (chain) {
 		case 3: //ropsten testnet
 			txUrl = `https://ropsten.etherscan.io/tx/${transaction.transactionHash}`;
 			break;
-		case 1: 
+		case 1:
 			txUrl = `https://etherscan.io/tx/${transaction.transactionHash}`;
 			break;
 		default:
 			txUrl = transaction.transactionHash;
 	}
-	console.log("Transaction sent successfully:\n" + txUrl );
-	
+	console.log("Transaction sent successfully:\n" + txUrl);
+
 	return transaction;
 }
 
