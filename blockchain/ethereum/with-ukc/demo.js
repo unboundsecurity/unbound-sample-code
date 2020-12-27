f568bcbebe1ec4477f80f1dec4cb1142f6ba6ae9
Adam Ilan <adam.ilan@unboundtech.com>
2020-12-27 15:33:02 +0200

    ukc-eth show message when no infura provider

--- blockchain/ethereum/with-ukc/demo.js
+++ blockchain/ethereum/with-ukc/demo.js
@@ -3,12 +3,17 @@ const UkcEthereumWallet = require('./ukc-ethereum-wallet');
 (async function run() {
 	try {
 		// get your ethereum ropsten provider endpoint by creating an account in https://infura.io
-		var wallet = new UkcEthereumWallet("https://<network>.infura.io/v3/YOUR-PROJECT-ID");
+		const web3ProviderUrl = "";
+		const ukcHost = "https://localhost";
+		if(!web3ProviderUrl) {
+			throw "Please set your web3 / Infura provider url, such as 'https://<network>.infura.io/v3/YOUR-PROJECT-ID'. Open account in https://infura.io"
+		}
+		var wallet = new UkcEthereumWallet(web3ProviderUrl, ukcHost);
 
 		await wallet.init();
 		await wallet.send({
 			to: '0xDC839532D5ab3e7Cc958b4F86d733CbD1617ee4A',
-			amountInEth: "0.0004"
+			amountInEth: "0.0001"
 		})
 	} catch (e) {
 		const ukcErrorData = e.response && e.response.data;
