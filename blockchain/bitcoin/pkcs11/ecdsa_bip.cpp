8e54311dd223e0aac3334565571377468fc42e90
Michael Kraitsberg <michael.kraitsberg@unboundtech.com>
2021-01-03 20:04:27 +0200

    bitcoin bip32 - p2

--- blockchain/bitcoin/pkcs11/ecdsa_bip.cpp
+++ blockchain/bitcoin/pkcs11/ecdsa_bip.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../dy_pkcs11.h"
+#include "dy_pkcs11.h"
 
 void halt(CK_RV rv)
 {
@@ -22,34 +22,38 @@ int main(int argc, char *argv[])
 	CK_ULONG ckk_ec = CKK_EC;
 
 	rv = C_Initialize(NULL);
-	if (rv != CKR_OK) halt(rv);
+	if (rv != CKR_OK)
+		halt(rv);
 	rv = C_OpenSession(slot, CKF_SERIAL_SESSION | CKF_RW_SESSION, NULL, NULL, &hSession);
-	if (rv != CKR_OK) halt(rv);
+	if (rv != CKR_OK)
+		halt(rv);
 
 	unsigned char seed_key[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
-	unsigned char k256_oid[] = { 0x06, 0x05, 0x2b, 0x81, 0x04, 0x00, 0x0a }; //SECP256K1 curve
+	unsigned char k256_oid[] = {0x06, 0x05, 0x2b, 0x81, 0x04, 0x00, 0x0a}; //SECP256K1 curve
 
 	// 1. Create generic secret object
-	CK_ATTRIBUTE t_new_seed_key[] =	{
-		{CKA_CLASS,     &cko_secret_key,     sizeof(CK_ULONG)},
-		{CKA_KEY_TYPE,  &ckk_generic_secret, sizeof(CK_ULONG)},
-		{CKA_TOKEN,     &ckTrue,            sizeof(CK_BBOOL)},
-		{CKA_DERIVE,    &ckTrue,            sizeof(CK_BBOOL)},
-		{CKA_VALUE,     seed_key,           sizeof(seed_key)},
+	CK_ATTRIBUTE t_new_seed_key[] = {
+		{CKA_CLASS, &cko_secret_key, sizeof(CK_ULONG)},
+		{CKA_KEY_TYPE, &ckk_generic_secret, sizeof(CK_ULONG)},
+		{CKA_TOKEN, &ckTrue, sizeof(CK_BBOOL)},
+		{CKA_DERIVE, &ckTrue, sizeof(CK_BBOOL)},
+		{CKA_VALUE, seed_key, sizeof(seed_key)},
 	};
 	CK_OBJECT_HANDLE seed_handle = 0;
-	test_rv(C_CreateObject(hSession, t_new_seed_key, sizeof(t_new_seed_key) / sizeof(CK_ATTRIBUTE), &seed_handle));
+	rv = C_CreateObject(hSession, t_new_seed_key, sizeof(t_new_seed_key) / sizeof(CK_ATTRIBUTE), &seed_handle);
+	if (rv != CKR_OK)
+		halt(rv);
 
 	// 2. Derive key using ECDSA BIP mechanism
-	CK_ATTRIBUTE t_new_ec_key[] =	{
-		{CKA_CLASS,     &cko_private_key,  sizeof(CK_ULONG)},
-		{CKA_KEY_TYPE,  &ckk_ec,           sizeof(CK_ULONG)},
-		{CKA_TOKEN,     &ckTrue,          sizeof(CK_BBOOL)},
-		{CKA_EC_PARAMS, k256_oid,          sizeof(k256_oid)},
-		{CKA_SIGN,      &ckTrue,          sizeof(CK_BBOOL)},
-		{CKA_DERIVE,    &ckTrue,          sizeof(CK_BBOOL)},
+	CK_ATTRIBUTE t_new_ec_key[] = {
+		{CKA_CLASS, &cko_private_key, sizeof(CK_ULONG)},
+		{CKA_KEY_TYPE, &ckk_ec, sizeof(CK_ULONG)},
+		{CKA_TOKEN, &ckTrue, sizeof(CK_BBOOL)},
+		{CKA_EC_PARAMS, k256_oid, sizeof(k256_oid)},
+		{CKA_SIGN, &ckTrue, sizeof(CK_BBOOL)},
+		{CKA_DERIVE, &ckTrue, sizeof(CK_BBOOL)},
 	};
-	CK_MECHANISM bip_mech = { DYCKM_DERIVE_ECDSA_BIP, NULL, 0 };
+	CK_MECHANISM bip_mech = {DYCKM_DERIVE_ECDSA_BIP, NULL, 0};
 	CK_OBJECT_HANDLE derived_handle = 0;
 	rv = C_DeriveKey(hSession, &bip_mech, seed_handle, t_new_ec_key, sizeof(t_new_ec_key) / sizeof(CK_ATTRIBUTE), &derived_handle);
 
