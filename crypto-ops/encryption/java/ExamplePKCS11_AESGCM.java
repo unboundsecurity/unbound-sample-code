6c463772ae52b9a310298af52e66de9c7c5d6674
alexpresman <alexander.presman@unboundtech.com>
2021-01-10 12:48:43 +0200

    Fix printouts and comments

--- crypto-ops/encryption/java/ExamplePKCS11_AESGCM.java
+++ crypto-ops/encryption/java/ExamplePKCS11_AESGCM.java
@@ -1,3 +1,5 @@
+package com.unboundtech;
+
 import com.dyadicsec.cryptoki.*;
 
 public class ExamplePKCS11_AESGCM
@@ -5,8 +7,13 @@ public class ExamplePKCS11_AESGCM
   public static void main(String [] args) throws Throwable
   {
     Library.C_Initialize();
+    //Open Session
+    System.out.println("Open Session");
     CK_SESSION_HANDLE hSession = Library.C_OpenSession(0, CK.CKF_RW_SESSION | CK.CKF_SERIAL_SESSION);
 
+
+    //Generate AES key
+    System.out.println("Generate AES key");
     int aesKeyHandle = Library.C_GenerateKey(hSession, new CK_MECHANISM(CK.CKM_AES_KEY_GEN),
       new CK_ATTRIBUTE[]
       {
@@ -17,15 +24,22 @@ public class ExamplePKCS11_AESGCM
       });
 
     String plainData = "TEST PLAIN DATA";
+
+    //Generate random IV
+    System.out.println("Generate random IV");
     byte[] iv = Library.C_GenerateRandom(hSession, 12);
 
     CK_GCM_PARAMS gcmParams = new CK_GCM_PARAMS();
     gcmParams.pIv = iv;
     gcmParams.ulTagBits = 96;
 
+    //Encrypt data
+    System.out.println("Encrypt data");
     Library.C_EncryptInit(hSession, new CK_MECHANISM(CK.CKM_AES_GCM,gcmParams), aesKeyHandle);
     byte[] encrypted = Library.C_Encrypt(hSession, plainData.getBytes());
 
+    //Decrypt data
+    System.out.println("Decrypt data");
     Library.C_DecryptInit(hSession, new CK_MECHANISM(CK.CKM_AES_GCM,gcmParams), aesKeyHandle);
 
     byte[] decrypted;
@@ -38,8 +52,11 @@ public class ExamplePKCS11_AESGCM
       throw new SecurityException("GCM decryption failure", e);
     }
 
+    // Check that decrypted is identical to plain data
+    System.out.println("Check that decrypted is identical to plain data");
     String test = new String(decrypted);
 
     if (!test.equals(plainData)) throw new AssertionError("Decrypted sata mismatch");
+    System.out.println("Check successful");
   }
 }
