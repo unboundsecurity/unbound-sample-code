695320da21c3305bf3911d34e2fb4f5b5fa9e8b5
alexpresman <alexander.presman@unboundtech.com>
2021-01-11 19:03:14 +0200

    fix key-management java samples

--- key-management/java/pkcs11/ECDSAGenerate.java
+++ key-management/java/pkcs11/ECDSAGenerate.java
@@ -1,20 +1,33 @@
+package com.unboundtech.key_management.pkcs11;
+
 import com.dyadicsec.cryptoki.*;
 
 public class ECDSAGenerate
 {
   static final byte[] P256_OID = new byte[] {0x06, 0x08, 0x2a, (byte)0x86, 0x48, (byte)0xce, 0x3d, 0x03, 0x01, 0x07};
 
-  public static void run() throws Exception
+  /***************
+   *
+   * @param args - if default user password is non-empty, provide it as first arg
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception
   {
+    char[] pwd = null;
+    if (args.length > 0) pwd = args[0].toCharArray();
+
+
     int slotId = 0;
     byte[] keyName = "TEST ECDSA KEY".getBytes("UTF-8");
     Library.C_Initialize();
 
     // Open PKCS#11 session
+    System.out.println("Open Session and login with default user");
     CK_SESSION_HANDLE session = Library.C_OpenSession(slotId, CK.CKF_RW_SESSION | CK.CKF_SERIAL_SESSION);
-    Library.C_Login(session, CK.CKU_USER, Main.password); // optional
+    Library.C_Login(session, CK.CKU_USER, pwd); // optional
 
     // Generate key pair
+    System.out.println("Generate key pair");
     int[] keyHandles = Library.C_GenerateKeyPair(session,
       new CK_MECHANISM(CK.CKM_EC_KEY_PAIR_GEN), // EC generation mechanism
       new CK_ATTRIBUTE[]
@@ -31,6 +44,7 @@ public class ECDSAGenerate
     assert keyHandles.length==2;
 
     // Find the ECDSA private key
+    System.out.println("Find the ECDSA private key");
     Library.C_FindObjectsInit(session, new CK_ATTRIBUTE[]
       {
         new CK_ATTRIBUTE(CK.CKA_TOKEN, true),
@@ -41,8 +55,10 @@ public class ECDSAGenerate
     int[] foundKeyHandles = Library.C_FindObjects(session, 1);
     Library.C_FindObjectsFinal(session);
     assert foundKeyHandles.length==1;
+    System.out.println("Key found - success");
 
     // Close PKCS#11 session
+    System.out.println("Close PKCS#11 session");
     Library.C_CloseSession(session);
     Library.C_Finalize();
   }
--- key-management/java/pkcs11/RSAGenerate.java
+++ key-management/java/pkcs11/RSAGenerate.java
@@ -1,18 +1,30 @@
+package com.unboundtech.key_management.pkcs11;
+
 import com.dyadicsec.cryptoki.*;
 
 public class RSAGenerate
 {
-  public static void run() throws Exception
+  /***************
+   *
+   * @param args - if default user password is non-empty, provide it as first arg
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception
   {
+    char[] pwd = null;
+    if (args.length > 0) pwd = args[0].toCharArray();
+
     int slotId = 0;
     byte[] keyName = "TEST RSA KEY".getBytes("UTF-8");
     Library.C_Initialize();
 
     // Open PKCS#11 session
+    System.out.println("Open Session and login with default user");
     CK_SESSION_HANDLE session = Library.C_OpenSession(slotId, CK.CKF_RW_SESSION | CK.CKF_SERIAL_SESSION);
-    Library.C_Login(session, CK.CKU_USER, Main.password); // optional
+    Library.C_Login(session, CK.CKU_USER, pwd); // optional
 
     // Generate key pair
+    System.out.println("Generate key pair");
     int[] keyHandles = Library.C_GenerateKeyPair(session,
       new CK_MECHANISM(CK.CKM_RSA_PKCS_KEY_PAIR_GEN), // RSA generation mechanism
       new CK_ATTRIBUTE[]
@@ -28,6 +40,7 @@ public class RSAGenerate
     assert keyHandles.length==2;
 
     // Find the RSA private key
+    System.out.println("Find the RSA private key");
     Library.C_FindObjectsInit(session, new CK_ATTRIBUTE[]
       {
         new CK_ATTRIBUTE(CK.CKA_TOKEN, true),
@@ -38,8 +51,10 @@ public class RSAGenerate
     int[] foundKeyHandles = Library.C_FindObjects(session, 1);
     Library.C_FindObjectsFinal(session);
     assert foundKeyHandles.length==1;
+    System.out.println("Key found - success");
 
     // Close PKCS#11 session
+    System.out.println("Close PKCS#11 session");
     Library.C_CloseSession(session);
     Library.C_Finalize();
   }
