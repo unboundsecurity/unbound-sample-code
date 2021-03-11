784d69c1601f5153730a5dee3c3fa8a86c67cebe
alexpresman <alexander.presman@unboundtech.com>
2021-03-11 13:52:22 +0200

    Fix server certificate validation

--- general/auth_with_client_certificate/c-sharp/login_and_encrypt.cs
+++ general/auth_with_client_certificate/c-sharp/login_and_encrypt.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
-/****************
+/**************************************************************************************
  * This class overloads standard WebClient to add a client certificate to the request 
  */
 class WebClientWithCert : WebClient
@@ -16,24 +16,22 @@ class WebClientWithCert : WebClient
   }
   protected override WebRequest GetWebRequest(Uri address)
   {
-    HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);    
+    HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);
     request.ClientCertificates.Add(certificate);
     return request;
   }
-  
+
 }
 
 public class EncryptData
 {
-  static void set_server_cert_validation(bool no_cert)
-  { 
-    if (no_cert)
-      ServicePointManager.ServerCertificateValidationCallback = (sender, cert, chain, errors) => true;
-    else //Validate server certificate
+  static void set_server_cert_validation(bool check_server_ca, string server_ca_pem)
+  {
+    if (check_server_ca)
     {
-      X509Certificate2 server_cert = new X509Certificate2("path/to/server/CA/file");
+      X509Certificate2 server_cert = new X509Certificate2(server_ca_pem);
       var validCerts = new Dictionary<X509Certificate2, string>()
-      { 
+      {
         { server_cert, server_cert.GetCertHashString() },
       };
       ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, errors) =>
@@ -43,6 +41,8 @@ public class EncryptData
           && validCerts.ContainsValue(server_root_ca.GetCertHashString());
       };
     }
+    else //Accept any certificate
+      ServicePointManager.ServerCertificateValidationCallback = (sender, cert, chain, errors) => true;
   }
   public static string base64_encode(string plainText)
   {
@@ -53,12 +53,12 @@ public class EncryptData
   static void set_request_headers(ref WebClientWithCert client)
   {
     client.Headers.Add("Content-Type", "application/json");
-    
+
     //Default UKC user
     const string default_ukc_user = "user";
     //Encode UKC user with colon (:) at end
     string default_ukc_user_base64 = base64_encode(default_ukc_user + ":");
-    
+
     //Add result to Authorization header
     client.Headers.Add("Authorization", $"Basic {default_ukc_user_base64}");
   }
@@ -66,26 +66,32 @@ public class EncryptData
   public static void Main(string[] args)
   {
     //Client certificate location and password
-    const string cert_file = "/path/to/client/certificate/file";
-    const string cert_pwd = "Password1!";
+    const string client_cert_file = "/path/to/client/certificate/file";
+    const string client_cert_pwd = "cert-file-password";
 
     //Set encryption parameters
-    const string key_name = "aes-for-enc"; //replace with key name in UKC to encrypt with
+    const string key_name = "aes-key"; //replace with key name in UKC to encrypt with
     const string partition = "test"; //replace with partition name where the key is located
     const string message = "sample message to encrypt";
     const string hostname = "localhost:8443"; //replace with server hostname and port
 
-    //set to false to validate server certificate
-    bool no_server_cert = true;
-    
-    set_server_cert_validation(no_server_cert);
+    /******************************************************
+     * Validate UKC server certificate (default: false)
+     * Set to true to validate server certificate.
+     * Important: If set to true and use Windows, you first need to install UKC server CA in Trusted Root CA directory of the Current User Certificates
+     */
+    bool check_server_cert = false;
+	//If check_server_cert is set to true, you need to obtain UKC server CA in pem format and set its path here
+    const string server_ca_pem = "path/to/server/CA/PEM/file";
 
-    //create web client with certificate, which overloads standard WebClient class)
-    WebClientWithCert client = new WebClientWithCert(cert_file, cert_pwd);
+    set_server_cert_validation(check_server_cert, server_ca_pem);
+
+    //create web client with client certificate, which overloads standard WebClient class
+    WebClientWithCert client = new WebClientWithCert(client_cert_file, client_cert_pwd);
 
     //set headers and credentials
     set_request_headers(ref client);
-    
+
     string uri_encrypt = $"https://{hostname}/api/v1/keys/{key_name}/encrypt?partitionId={partition}";
     string body_encrypt = $"{{\"clearText\": \"{message}\",\"dataEncoding\": \"PLAIN\"}}";
 
