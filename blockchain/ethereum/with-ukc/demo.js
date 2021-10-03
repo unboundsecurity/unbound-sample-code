9799ce2f914ac6cadff74ce33d741bd40382ae9d
Adam Ilan <adam.ilan@unboundtech.com>
2021-10-03 14:44:41 +0300

    fix vulnarabilities

--- blockchain/ethereum/with-ukc/demo.js
+++ blockchain/ethereum/with-ukc/demo.js
@@ -3,8 +3,8 @@ const UkcEthereumWallet = require('./ukc-ethereum-wallet');
 (async function run() {
 	try {
 		// get your ethereum ropsten provider endpoint by creating an account in https://infura.io
-		const web3ProviderUrl = "";
-		const ukcHost = "https://localhost";
+		const web3ProviderUrl = "https://ropsten.infura.io/v3/eece94a9579c406a84634e0f0d42d1c3";
+		const ukcHost = "https://localhost:8443";
 		if(!web3ProviderUrl) {
 			throw "Please set your web3 / Infura provider url, such as 'https://<network>.infura.io/v3/YOUR-PROJECT-ID'. Open account in https://infura.io"
 		}
